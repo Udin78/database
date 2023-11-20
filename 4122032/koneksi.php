ed64c98a87ee4f83f425133253b8df4b391b0d9a
ariff <121781089+Udin78@users.noreply.github.com>
2023-11-20 12:52:11 +0700

    Add files via upload

---
+++ 4122032/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host ="localhost";
+$user ="root";
+$password = "4122032";
+$db = "Sekolahanku";
+$koneksi = mysqli_connect( "127.0.0.1", "root", "Sekolahanku");
+// ->>>>> "server", "user", "password", "nama database"
+?>
