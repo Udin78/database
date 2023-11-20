21fa3c199b4de1cc9935c0fcc2d4f810e4856ee1
ariff <121781089+Udin78@users.noreply.github.com>
2023-11-20 23:22:15 +0700

    koneksi.php

--- 4122032/koneksi.php
+++ 4122032/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host ="localhost";
 $user ="root";
-$password = "4122032";
-$db = "Sekolahanku";
-$koneksi = mysqli_connect( "127.0.0.1", "root", "Sekolahanku");
+$password = "";
+$db = "Sekolah";
+$koneksi = mysqli_connect( "127.0.0.1", "root","", "Sekolah");
 // ->>>>> "server", "user", "password", "nama database"
 ?>
